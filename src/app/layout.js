3ec42383a75f3ff5fae47b13313ecd0898e36cfc
Muhammad Huzaifah <mhuzaifah989@gmail.com>
2025-01-01 04:54:03 -0500

    favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,3 @@
-'use client';
 import '../styles/globals.css';
 import { MantineProvider } from '@mantine/core';
 import Header from '../components/Header/Header';
@@ -13,12 +12,19 @@ const openSans = Open_Sans({
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
+            <head>
+                <link rel="icon" href="/favicon.ico" sizes="any"/>
+                <link rel="apple-touch-icon" href="/apple-touch-icon.png"/>
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+                <title></title>
+            </head>
             <body className={openSans.className}>
-                <MantineProvider withGlobalStyles withNormalizeCSS>
-                    <Header />
-                        {children}
-                    <Footer />
-                </MantineProvider>
+            <MantineProvider withGlobalStyles withNormalizeCSS>
+                <Header/>
+                {children}
+                <Footer/>
+            </MantineProvider>
             </body>
         </html>
     );
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,17 @@
+'use client';
 import '../styles/main.css';
+import React, {useEffect} from 'react';
 import Hero from '../components/Hero/Hero';
 import About from '../components/About/About';
 import Projects from '../components/Projects/Projects';
 import Contact from '../components/Contact/Contact';
 
 export default function App() {
+
+    useEffect(() => {
+        document.title = "Muhammad Huzaifah Portfolio";
+    }, []);
+
     return (
         <main>
             <section id="hero">
